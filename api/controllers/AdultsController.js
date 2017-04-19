b6d3b0fc8b9a82904fb9280bba5e4d8f49659b75
Caleb Kim <calebykim@MacBook-Pro-7.local>
2017-04-19 17:26:36 -0400

    integrated mySql's, made adults and family tables

---
+++ api/controllers/AdultsController.js
@@ -0,0 +1,12 @@
+/**
+ * AdultsController
+ *
+ * @description :: Server-side logic for managing adults
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+
+};
+
---
+++ api/models/Adults.js
@@ -0,0 +1,30 @@
+/**
+ * Adults.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    id: {
+      type: 'integer',
+      autoIncrement: true,
+      primaryKey: true
+    },
+    family_id:{
+      type: 'integer',
+      autoIncrement: true
+    },
+    email:{
+      type: 'string'
+    },
+    savings_balance:{
+      type: 'float'
+    },
+    checking_balance:{
+      type: 'float'
+    }
+  }
+};
+
