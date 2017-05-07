bfb1b8127131e09d9a534ad3a70e53288ba9dc35
joeyoo <joseph_yoo@brown.edu>
2017-05-07 16:50:24 -0400

    remove transactions and replace with transfers

--- config/routes.js
+++ config/routes.js
@@ -56,11 +56,6 @@ module.exports.routes = {
   'GET /families/:id/edit': 'FamilyController.edit',
   'POST /famlies': 'FamilyController.create',
   'PUT /families/:id': 'FamilyController.update',
-  // transactions
-  'GET /transactions/new': 'TransactionController.new',
-  'GET /transactions/:id': 'TransactionController.show',
-  'POST /transactions': 'TransactionController.create',
-
   // Deposit
   'GET /deposits/new': 'DepositController.new',
   'POST /deposits': 'DepositController.create',
